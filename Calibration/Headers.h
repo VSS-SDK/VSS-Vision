6cf413b4c22d8391e35ccda592fb3b86aa052074
sirlab <sirlab.faeterj@gmail.com>
2016-04-16 12:01:05 -0300

    correções

--- Calibration/Headers.h
+++ Calibration/Headers.h
@@ -2,7 +2,7 @@
  * This file is part of the VSS-Vision project.
  *
  * This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
- * v. 3.0. If a copy of the MPL was not distributed with this
+ * v. 3.0. If a copy of the GPL was not distributed with this
  * file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
  */
  
