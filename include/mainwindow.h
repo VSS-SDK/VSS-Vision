f11da554e8b8ded0252d11020f517dbe2ad8e807
Alexandre <alexandremstf@gmail.com>
2017-12-10 18:56:32 -0200

    Atualizando branch cmake com alterações da janela

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -12,6 +12,31 @@ private:
 	Gtk::Window* window = nullptr;
 	Gtk::FileChooserButton* button_load = nullptr;
 	
+ 	Gtk::ComboBoxText* color_selected = nullptr;
+	Gtk::ComboBoxText* color_team_1 = nullptr;
+	Gtk::ComboBoxText* color_team_2 = nullptr;
+	Gtk::ComboBoxText* color_robot_1 = nullptr;
+	Gtk::ComboBoxText* color_robot_2 = nullptr;
+	Gtk::ComboBoxText* color_robot_3 = nullptr;
+	Gtk::ComboBoxText* input_path = nullptr;
+
+	Gtk::Scale* scale_h_max = nullptr;
+	Gtk::Scale* scale_h_min = nullptr;
+	Gtk::Scale* scale_s_max = nullptr;
+	Gtk::Scale* scale_s_min = nullptr;
+	Gtk::Scale* scale_v_max = nullptr;
+	Gtk::Scale* scale_v_min = nullptr;
+	Gtk::Scale* scale_rotation;
+	Gtk::Scale* scale_brightness;
+	Gtk::Scale* scale_contrast;
+	Gtk::Scale* scale_exposure;
+	Gtk::Scale* scale_saturation;
+	Gtk::Scale* scale_gain;
+
+	Gtk::RadioButton* radio_button_image;
+	Gtk::RadioButton* radio_button_video;
+	Gtk::RadioButton* radio_button_camera;
+	
 	
 	void set_widget_signal();
 	void load_widget_from_file();
@@ -19,6 +44,31 @@ private:
 
 	bool on_keyboard(GdkEventKey*);
 	void on_button_load();
+
+	void on_color_selected();
+	void on_color_team_1();
+	void on_color_team_2();
+	void on_color_robot_1();
+	void on_color_robot_2();
+	void on_color_robot_3();
+	void on_input_path();
+
+	void on_scale_h_max();
+	void on_scale_h_min();
+	void on_scale_s_max();
+	void on_scale_s_min();
+	void on_scale_v_max();
+	void on_scale_v_min();
+	void on_scale_rotation();
+	void on_scale_brightness();
+	void on_scale_contrast();
+	void on_scale_saturation();
+	void on_scale_exposure();
+	void on_scale_gain();
+
+	void on_radio_button_image();
+	void on_radio_button_video();
+	void on_radio_button_camera();
 
 public:
 	MainWindow();
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -22,8 +22,6 @@ void MainWindow::load_widget_from_file(){
 
 	try {
 		builder->add_from_file("../glade/mainwindow.glade");
-		builder->get_widget("window", window);
-		builder->get_widget("button_load", button_load);
 	} catch(const Glib::FileError& ex) {
 		std::cerr << "FileError: " << ex.what() << std::endl;
 	} catch(const Glib::MarkupError& ex) {
@@ -31,16 +29,72 @@ void MainWindow::load_widget_from_file(){
 	} catch(const Gtk::BuilderError& ex) {
 		std::cerr << "BuilderError: " << ex.what() << std::endl;
 	}
+	
+	builder->get_widget("window", window);
+	builder->get_widget("button_load", button_load);
+
+	builder->get_widget("color_selected", color_selected);
+	builder->get_widget("color_team_1", color_team_1);
+	builder->get_widget("color_team_2", color_team_2);
+	builder->get_widget("color_robot_1", color_robot_1);
+	builder->get_widget("color_robot_2", color_robot_2);
+	builder->get_widget("color_robot_3", color_robot_3);
+	builder->get_widget("combo_box_input_path", input_path);
+
+	builder->get_widget("scale_h_max", scale_h_max);
+	builder->get_widget("scale_h_min", scale_h_min);
+	builder->get_widget("scale_s_max", scale_s_max);
+	builder->get_widget("scale_s_min", scale_s_min);
+	builder->get_widget("scale_v_max", scale_v_max);
+	builder->get_widget("scale_v_min", scale_v_min);
+	builder->get_widget("scale_rotation", scale_rotation);
+	builder->get_widget("scale_brightness", scale_brightness);
+	builder->get_widget("scale_contrast", scale_contrast);
+	builder->get_widget("scale_exposure", scale_exposure);
+	builder->get_widget("scale_saturation", scale_saturation);
+	builder->get_widget("scale_gain", scale_gain);
+
+	builder->get_widget("radio_button_image", radio_button_image);    
+	builder->get_widget("radio_button_video", radio_button_video);    
+	builder->get_widget("radio_button_camera", radio_button_camera);    
+	
+
 }
 
 void MainWindow::set_widget_signal(){
 	window->signal_key_press_event().connect(sigc::mem_fun(this, &MainWindow::on_keyboard), false);
-	button_load->signal_file_set().connect( sigc::mem_fun(this, &MainWindow::on_button_load) );
+	button_load->signal_file_set().connect(sigc::mem_fun(this, &MainWindow::on_button_load) );
 
+	color_selected->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_selected));
+	color_team_1->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_team_1));
+	color_team_2->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_team_2));
+	color_robot_1->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_robot_1));
+	color_robot_2->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_robot_2));
+	color_robot_3->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_robot_3));
+	input_path->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_input_path));
+
+	scale_h_max->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_h_max));
+ 	scale_h_min->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_h_min));
+	scale_s_max->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_s_max));
+	scale_s_min->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_s_min));
+	scale_v_max->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_v_max));
+	scale_v_min->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_v_min));
+	scale_rotation->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_rotation));
+	scale_brightness->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_brightness));
+	scale_contrast->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_contrast));
+	scale_exposure->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_exposure));
+	scale_saturation->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_saturation));
+	scale_gain->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_gain));
+
+    radio_button_image->signal_pressed().connect(sigc::mem_fun(this, &MainWindow::on_radio_button_image));
+    radio_button_video->signal_pressed().connect(sigc::mem_fun(this, &MainWindow::on_radio_button_video));
+    radio_button_camera->signal_pressed().connect(sigc::mem_fun(this, &MainWindow::on_radio_button_camera));
+    
 }
 
 void MainWindow::initialize_widget(){
 	window->maximize();
+	radio_button_image->set_active();
 }
 
 bool MainWindow::on_keyboard(GdkEventKey* event){
@@ -56,4 +110,94 @@ bool MainWindow::on_keyboard(GdkEventKey* event){
 
 void MainWindow::on_button_load(){
 	cout << button_load->get_filename() << endl;
+}
+
+void MainWindow::on_color_selected(){
+	cout << color_selected->get_active_text() << endl;
+}
+
+void MainWindow::on_color_team_1(){
+	cout << color_team_1->get_active_text() << endl;
+}
+
+void MainWindow::on_color_team_2(){
+	cout << color_team_2->get_active_text() << endl;
+}
+
+void MainWindow::on_color_robot_1(){
+	cout << color_robot_1->get_active_text() << endl;
+}
+
+void MainWindow::on_color_robot_2(){
+	cout << color_robot_2->get_active_text() << endl;
+}
+
+void MainWindow::on_color_robot_3(){
+	cout << color_robot_3->get_active_text() << endl;
+}
+
+void MainWindow::on_input_path(){
+	cout << input_path->get_active_text() << endl;
+}
+
+void MainWindow::on_scale_h_max(){
+	cout << scale_h_max->get_value() << endl;	
+}
+
+void MainWindow::on_scale_h_min(){
+	cout << scale_h_min->get_value() << endl;	
+}
+
+void MainWindow::on_scale_s_max(){
+	cout << scale_s_max->get_value() << endl;	
+}
+
+void MainWindow::on_scale_s_min(){
+	cout << scale_s_min->get_value() << endl;	
+}
+
+void MainWindow::on_scale_v_max(){
+	cout << scale_v_max->get_value() << endl;	
+}
+
+void MainWindow::on_scale_v_min(){
+	cout << scale_v_min->get_value() << endl;	
+}
+
+void MainWindow::on_scale_rotation(){
+	cout << scale_rotation->get_value() << endl;
+}
+
+void MainWindow::on_scale_brightness(){
+	cout << scale_brightness->get_value() << endl;
+}
+
+void MainWindow::on_scale_contrast(){
+	cout << scale_contrast->get_value() << endl;
+}
+
+void MainWindow::on_scale_saturation(){
+	cout << scale_saturation->get_value() << endl;
+}
+
+void MainWindow::on_scale_exposure(){
+	cout << scale_exposure->get_value() << endl;
+}
+
+void MainWindow::on_scale_gain(){
+	cout << scale_gain->get_value() << endl;
+}
+
+
+void MainWindow::on_radio_button_image(){
+	if (!radio_button_image->get_active())
+		cout << "Image: " << radio_button_image->get_active() << endl;
+}
+void MainWindow::on_radio_button_video(){
+	if (!radio_button_video->get_active())
+		cout << "Video: " << radio_button_video->get_active() << endl;
+}
+void MainWindow::on_radio_button_camera(){
+	if (!radio_button_camera->get_active())
+		cout << "Camera: " << radio_button_camera->get_active() << endl;
 }
