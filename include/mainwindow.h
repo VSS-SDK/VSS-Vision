e5f6355bb8fe00e1930538f55e9d4705e91d1c7c
Alexandre <alexandremstf@gmail.com>
2017-12-11 16:12:54 -0200

    Widgets com métodos e sinais linkados, criação de janela para LOAD/SAVE arquivos, comentário no código

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -9,66 +9,92 @@ using namespace std;
 class MainWindow {
 
 private:
-	Gtk::Window* window = nullptr;
-	Gtk::FileChooserButton* button_load = nullptr;
 	
- 	Gtk::ComboBoxText* color_selected = nullptr;
+// Window - save/load file 
+	Gtk::Window* window_file = nullptr;
+
+	// Button
+	Gtk::Button* button_save = nullptr;
+	Gtk::Button* button_load = nullptr;
+
+	// File Chooser
+	Gtk::FileChooserWidget* file_chooser = nullptr;
+
+
+// Window - calibration
+	Gtk::Window* window = nullptr;
+	
+	// Button
+	Gtk::Button* button_window_file = nullptr;
+	Gtk::RadioButton* radio_button_image = nullptr;
+	Gtk::RadioButton* radio_button_video = nullptr;
+	Gtk::RadioButton* radio_button_camera = nullptr;
+
+	// Select text
+	Gtk::ComboBoxText* input_path = nullptr;
 	Gtk::ComboBoxText* color_team_1 = nullptr;
 	Gtk::ComboBoxText* color_team_2 = nullptr;
 	Gtk::ComboBoxText* color_robot_1 = nullptr;
 	Gtk::ComboBoxText* color_robot_2 = nullptr;
 	Gtk::ComboBoxText* color_robot_3 = nullptr;
-	Gtk::ComboBoxText* input_path = nullptr;
+	Gtk::ComboBoxText* color_selected = nullptr;
 
+	// Slider
 	Gtk::Scale* scale_h_max = nullptr;
 	Gtk::Scale* scale_h_min = nullptr;
 	Gtk::Scale* scale_s_max = nullptr;
 	Gtk::Scale* scale_s_min = nullptr;
 	Gtk::Scale* scale_v_max = nullptr;
 	Gtk::Scale* scale_v_min = nullptr;
-	Gtk::Scale* scale_rotation;
-	Gtk::Scale* scale_brightness;
-	Gtk::Scale* scale_contrast;
-	Gtk::Scale* scale_exposure;
-	Gtk::Scale* scale_saturation;
-	Gtk::Scale* scale_gain;
+	Gtk::Scale* scale_gain  = nullptr;
+	Gtk::Scale* scale_rotation = nullptr;
+	Gtk::Scale* scale_contrast = nullptr;
+	Gtk::Scale* scale_exposure = nullptr;
+	Gtk::Scale* scale_brightness = nullptr;
+	Gtk::Scale* scale_saturation = nullptr;
 
-	Gtk::RadioButton* radio_button_image;
-	Gtk::RadioButton* radio_button_video;
-	Gtk::RadioButton* radio_button_camera;
+<<<<<<< HEAD
+// Action method
+	bool on_keyboard(GdkEventKey*);
 	
-	
-	void set_widget_signal();
-	void load_widget_from_file();
-	void initialize_widget();
-
+	void on_input_path();
+=======
 	bool on_keyboard(GdkEventKey *event);
 	void on_button_load();
 
 	void on_color_selected();
+>>>>>>> 390d5a84890325662ccbcb9afb4b4c0b2d97caf5
 	void on_color_team_1();
 	void on_color_team_2();
 	void on_color_robot_1();
 	void on_color_robot_2();
 	void on_color_robot_3();
-	void on_input_path();
-
+	void on_color_selected();
+	
 	void on_scale_h_max();
 	void on_scale_h_min();
 	void on_scale_s_max();
 	void on_scale_s_min();
 	void on_scale_v_max();
 	void on_scale_v_min();
-	void on_scale_rotation();
-	void on_scale_brightness();
-	void on_scale_contrast();
-	void on_scale_saturation();
-	void on_scale_exposure();
 	void on_scale_gain();
-
+	void on_scale_rotation();
+	void on_scale_contrast();
+	void on_scale_exposure();
+	void on_scale_saturation();
+	void on_scale_brightness();
+	
+	void on_button_load();
+	void on_button_save();
+	void on_button_window_file();
 	void on_radio_button_image();
 	void on_radio_button_video();
 	void on_radio_button_camera();
+
+// Control method
+	void initialize_widget();
+	void set_widget_signal();
+	void load_widget_from_file();
 
 public:
 	MainWindow();
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 
+/*
+ * Constructor class
+ */
 MainWindow::MainWindow(){
 }
 
+/*
+ * Destructor class
+ */
 MainWindow::~MainWindow(){
 }
 
+/*
+ * Method to create and run the aplication
+ */
 void MainWindow::run(){
 	auto app = Gtk::Application::create();
 
@@ -16,62 +25,77 @@ void MainWindow::run(){
 	app->run(*window);
 }
 
+/*
+ * Method to load all widgets created in glade
+ */
 void MainWindow::load_widget_from_file(){
 
 	auto builder = Gtk::Builder::create();
 
 	try {
 		builder->add_from_file("../glade/mainwindow.glade");
+
+		builder->get_widget("window", window);
+		
+		builder->get_widget("window_file", window_file);
+		builder->get_widget("button_save", button_save);
+		builder->get_widget("button_load", button_load);
+		builder->get_widget("button_window_file", button_window_file);
+		builder->get_widget("file_chooser", file_chooser);
+	
+		builder->get_widget("color_selected", color_selected);
+		builder->get_widget("color_team_1", color_team_1);
+		builder->get_widget("color_team_2", color_team_2);
+		builder->get_widget("color_robot_1", color_robot_1);
+		builder->get_widget("color_robot_2", color_robot_2);
+		builder->get_widget("color_robot_3", color_robot_3);
+		builder->get_widget("combo_box_input_path", input_path);
+	
+		builder->get_widget("scale_h_max", scale_h_max);
+		builder->get_widget("scale_h_min", scale_h_min);
+		builder->get_widget("scale_s_max", scale_s_max);
+		builder->get_widget("scale_s_min", scale_s_min);
+		builder->get_widget("scale_v_max", scale_v_max);
+		builder->get_widget("scale_v_min", scale_v_min);
+		builder->get_widget("scale_rotation", scale_rotation);
+		builder->get_widget("scale_brightness", scale_brightness);
+		builder->get_widget("scale_contrast", scale_contrast);
+		builder->get_widget("scale_exposure", scale_exposure);
+		builder->get_widget("scale_saturation", scale_saturation);
+		builder->get_widget("scale_gain", scale_gain);
+	
+		builder->get_widget("radio_button_image", radio_button_image);    
+		builder->get_widget("radio_button_video", radio_button_video);    
+		builder->get_widget("radio_button_camera", radio_button_camera);    
+			
 	} catch(const Glib::FileError& ex) {
 		std::cerr << "FileError: " << ex.what() << std::endl;
+
 	} catch(const Glib::MarkupError& ex) {
 		std::cerr << "MarkupError: " << ex.what() << std::endl;
+
 	} catch(const Gtk::BuilderError& ex) {
 		std::cerr << "BuilderError: " << ex.what() << std::endl;
 	}
-	
-	builder->get_widget("window", window);
-	builder->get_widget("button_load", button_load);
-
-	builder->get_widget("color_selected", color_selected);
-	builder->get_widget("color_team_1", color_team_1);
-	builder->get_widget("color_team_2", color_team_2);
-	builder->get_widget("color_robot_1", color_robot_1);
-	builder->get_widget("color_robot_2", color_robot_2);
-	builder->get_widget("color_robot_3", color_robot_3);
-	builder->get_widget("combo_box_input_path", input_path);
-
-	builder->get_widget("scale_h_max", scale_h_max);
-	builder->get_widget("scale_h_min", scale_h_min);
-	builder->get_widget("scale_s_max", scale_s_max);
-	builder->get_widget("scale_s_min", scale_s_min);
-	builder->get_widget("scale_v_max", scale_v_max);
-	builder->get_widget("scale_v_min", scale_v_min);
-	builder->get_widget("scale_rotation", scale_rotation);
-	builder->get_widget("scale_brightness", scale_brightness);
-	builder->get_widget("scale_contrast", scale_contrast);
-	builder->get_widget("scale_exposure", scale_exposure);
-	builder->get_widget("scale_saturation", scale_saturation);
-	builder->get_widget("scale_gain", scale_gain);
-
-	builder->get_widget("radio_button_image", radio_button_image);    
-	builder->get_widget("radio_button_video", radio_button_video);    
-	builder->get_widget("radio_button_camera", radio_button_camera);    
-	
-
 }
 
+/*
+ * Set the correspondent signal/action to the method
+ */
 void MainWindow::set_widget_signal(){
 	window->signal_key_press_event().connect(sigc::mem_fun(this, &MainWindow::on_keyboard), false);
-	button_load->signal_file_set().connect(sigc::mem_fun(this, &MainWindow::on_button_load) );
+	
+	button_load->signal_clicked().connect(sigc::mem_fun(this, &MainWindow::on_button_load) );
+	button_save->signal_clicked().connect(sigc::mem_fun(this, &MainWindow::on_button_save) );
+	button_window_file->signal_clicked().connect(sigc::mem_fun(this, &MainWindow::on_button_window_file));
 
+	input_path	  ->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_input_path));
+	color_team_1  ->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_team_1));
+	color_team_2  ->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_team_2));
+	color_robot_1 ->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_robot_1));
+	color_robot_2 ->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_robot_2));
+	color_robot_3 ->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_robot_3));
 	color_selected->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_selected));
-	color_team_1->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_team_1));
-	color_team_2->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_team_2));
-	color_robot_1->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_robot_1));
-	color_robot_2->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_robot_2));
-	color_robot_3->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_color_robot_3));
-	input_path->signal_changed().connect(sigc::mem_fun(this, &MainWindow::on_input_path));
 
 	scale_h_max->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_h_max));
  	scale_h_min->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_h_min));
@@ -79,23 +103,30 @@ void MainWindow::set_widget_signal(){
 	scale_s_min->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_s_min));
 	scale_v_max->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_v_max));
 	scale_v_min->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_v_min));
-	scale_rotation->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_rotation));
-	scale_brightness->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_brightness));
-	scale_contrast->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_contrast));
-	scale_exposure->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_exposure));
-	scale_saturation->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_saturation));
-	scale_gain->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_gain));
 
-    radio_button_image->signal_pressed().connect(sigc::mem_fun(this, &MainWindow::on_radio_button_image));
-    radio_button_video->signal_pressed().connect(sigc::mem_fun(this, &MainWindow::on_radio_button_video));
+	scale_gain		->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_gain));
+	scale_rotation	->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_rotation));
+	scale_contrast	->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_contrast));
+	scale_exposure	->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_exposure));
+	scale_brightness->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_brightness));
+	scale_saturation->signal_value_changed().connect(sigc::mem_fun(this, &MainWindow::on_scale_saturation));
+
+    radio_button_image ->signal_pressed().connect(sigc::mem_fun(this, &MainWindow::on_radio_button_image));
+    radio_button_video ->signal_pressed().connect(sigc::mem_fun(this, &MainWindow::on_radio_button_video));
     radio_button_camera->signal_pressed().connect(sigc::mem_fun(this, &MainWindow::on_radio_button_camera));
-    
 }
 
+/*
+ * Initialize widgets according user needs
+ */
 void MainWindow::initialize_widget(){
 	window->maximize();
 	radio_button_image->set_active();
 }
+
+/*
+ * All methods bellow convert the user action in code
+ */
 
 bool MainWindow::on_keyboard(GdkEventKey* event){
 	if(event->keyval == GDK_KEY_space) {
@@ -109,7 +140,15 @@ bool MainWindow::on_keyboard(GdkEventKey* event){
 }
 
 void MainWindow::on_button_load(){
-	cout << button_load->get_filename() << endl;
+	cout << file_chooser->get_filename() << endl;
+}
+
+void MainWindow::on_button_save(){
+	cout << file_chooser->get_filename() << endl;
+}
+
+void MainWindow::on_button_window_file(){
+	window_file->show();
 }
 
 void MainWindow::on_color_selected(){
@@ -187,7 +226,6 @@ void MainWindow::on_scale_exposure(){
 void MainWindow::on_scale_gain(){
 	cout << scale_gain->get_value() << endl;
 }
-
 
 void MainWindow::on_radio_button_image(){
 	if (!radio_button_image->get_active())
