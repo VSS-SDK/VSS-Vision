f4024b885d4b0d78cf53585e01206964c0ddddaf
John Fercher <johnathanfercher22@gmail.com>
2018-02-03 13:50:12 -0200

    test

---
+++ include/Core/DirectoryHelper.h
@@ -0,0 +1,14 @@
+//
+// Created by johnathan on 03/02/18.
+//
+
+#ifndef VSS_VISION_DIRECTORYHELPER_H
+#define VSS_VISION_DIRECTORYHELPER_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include<iostream>
+
+std::string getCurrentWorkingDir( void );
+
+#endif //VSS_VISION_DIRECTORYHELPER_H
