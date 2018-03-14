d8370736fe0fd0d6806607fdc4154cad7379029e
Alexandre <alexandremstf@gmail.com>
2018-03-14 12:08:34 -0300

    Criando arquivos necessários para tela do vision

---
+++ include/Windows/Vision/VisionWindow.h
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the VSS-Vision project.
+ *
+ * This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
+ * v. 3.0. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
+ */
+
+#ifndef VISION_WINDOW_H_
+#define VISION_WINDOW_H_
+
+#include <gtkmm.h>
+#include <iostream>
+
+using namespace std;
+
+class VisionWindow {
+public:
+    VisionWindow();
+    virtual ~VisionWindow();
+
+private:
+
+};
+#endif
