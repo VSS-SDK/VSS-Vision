071cc0b1e6ee187ded1cf0eec56acc0811ea2e47
Paulo Henrique Santos <paulosantosphs@hotmail.com>
2018-05-07 13:31:16 -0300

    acertando vector

--- src/Windows/Vision/PartialVisionWindowEvents.cpp
+++ src/Windows/Vision/PartialVisionWindowEvents.cpp
@@ -8,12 +8,12 @@
 
 #include <Windows/Vision/VisionWindow.h>
 
-bool VisionWindow::onKeyboard(GdkEventKey* event, Gtk::Window*) {
-    if(event->keyval == GDK_KEY_space) {
+bool VisionWindow::onKeyboard(GdkEventKey *event, Gtk::Window *) {
+    if (event->keyval == GDK_KEY_space) {
         window->hide();
-    } else if(event->keyval == GDK_KEY_Return) {
+    } else if (event->keyval == GDK_KEY_Return) {
         window->hide();
-    } else if(event->keyval == GDK_KEY_Escape) {
+    } else if (event->keyval == GDK_KEY_Escape) {
         window->hide();
     }
     return true;
@@ -23,95 +23,158 @@ void VisionWindow::onButtonPlay() {
 
 }
 
-void VisionWindow::onButtonLoad(Gtk::FileChooserDialog* fileChooser, Gtk::Entry* entry) {
-    if (entry->get_text_length() > 0){
-    //calibration = calibrationRepository->read(file_chooser->get_filename());
-    fileChooser->hide();
-  }
+void VisionWindow::onButtonLoad(Gtk::FileChooserDialog *fileChooser, Gtk::Entry *entry) {
+    if (entry->get_text_length() > 0) {
+        //calibration = calibrationRepository->read(file_chooser->get_filename());
+        fileChooser->hide();
+    }
 }
 
-void VisionWindow::onButtonOpenLoadDialog(Gtk::FileChooserDialog* fileChooser, Gtk::Entry* entry) {
+void VisionWindow::onButtonOpenLoadDialog(Gtk::FileChooserDialog *fileChooser, Gtk::Entry *entry) {
     entry->set_sensitive(true);
     fileChooser->show();
 }
 
-void VisionWindow::onRadioButtonImage(Gtk::RadioButton* radioButton) {
+void VisionWindow::onRadioButtonImage(Gtk::RadioButton *radioButton) {
 //    if (!radioButton->get_active())
 //        std::cout << "Image: " << radioButton->get_active() << std::endl;
 }
 
-void VisionWindow::onRadioButtonVideo(Gtk::RadioButton* radioButton) {
+void VisionWindow::onRadioButtonVideo(Gtk::RadioButton *radioButton) {
 //    if (!radioButton->get_active())
 //        std::cout << "Video: " << radioButton->get_active() << std::endl;
 }
 
-void VisionWindow::onRadioButtonCamera(Gtk::RadioButton* radioButton) {
+void VisionWindow::onRadioButtonCamera(Gtk::RadioButton *radioButton) {
 //    if (!radioButton->get_active())
 //        std::cout << "Camera: " << radioButton->get_active() << std::endl;
 }
 
-void VisionWindow::onSignalSelectFileInDialog(Gtk::FileChooserDialog* fileChooser, Gtk::Entry* entry) {
+void VisionWindow::onSignalSelectFileInDialog(Gtk::FileChooserDialog *fileChooser, Gtk::Entry *entry) {
     std::string str = fileChooser->get_filename();
     std::size_t sub_str = str.find_last_of("/\\");
-    entry->set_text(str.substr(sub_str+1));
+    entry->set_text(str.substr(sub_str + 1));
 }
 
-void VisionWindow::onComboBoxSelectPath(Gtk::ComboBox* combobox) {
+void VisionWindow::onComboBoxSelectPath(Gtk::ComboBox *combobox) {
 //  std::cout << combobox->get_active_row_number() << std::endl;
 }
 
-void VisionWindow::onComboBoxSelectColorTeam1(Gtk::ComboBox* combobox) {
+void VisionWindow::onComboBoxSelectColorTeam1(Gtk::ComboBox *combobox) {
     vector<string> color = {"Blue", "Yellow"};
     ColorType colorType = toColorType(color[combobox->get_active_row_number()]);
     WhoseName whoseName = WhoseName::Team1;
     WhoseColor whoseColor(whoseName, colorType);
-    objects.push_back(whoseColor);
+    bool flag = false;
+    for (unsigned i = 0; i < objects.size(); i++) {
+        if (whoseName == objects[i].whoseName) {
+            objects[i] = whoseColor;
+            flag = true;
+        }
+    }
+    if (!flag) {
+        objects.push_back(whoseColor);
+    }
 }
 
-void VisionWindow::onComboBoxSelectColorTeam2(Gtk::ComboBox* combobox) {
+void VisionWindow::onComboBoxSelectColorTeam2(Gtk::ComboBox *combobox) {
     vector<string> color = {"Blue", "Yellow"};
     ColorType colorType = toColorType(color[combobox->get_active_row_number()]);
     WhoseName whoseName = WhoseName::Team2;
     WhoseColor whoseColor(whoseName, colorType);
-    objects.push_back(whoseColor);
+    bool flag = false;
+    for (unsigned i = 0; i < objects.size(); i++) {
+        if (whoseName == objects[i].whoseName) {
+            objects[i] = whoseColor;
+            flag = true;
+        }
+    }
+    if (!flag) {
+        objects.push_back(whoseColor);
+    }
 }
 
-void VisionWindow::onComboBoxSelectColorRobot1(Gtk::ComboBox* combobox) {
+void VisionWindow::onComboBoxSelectColorRobot1(Gtk::ComboBox *combobox) {
     vector<string> color = {"Green", "Pink", "Purple", "Red", "Brown"};
     ColorType colorType = toColorType(color[combobox->get_active_row_number()]);
     WhoseName whoseName = WhoseName::Robot1;
     WhoseColor whoseColor(whoseName, colorType);
-    objects.push_back(whoseColor);
+    bool flag = false;
+    for (unsigned i = 0; i < objects.size(); i++) {
+        if (whoseName == objects[i].whoseName) {
+            objects[i] = whoseColor;
+            flag = true;
+        }
+    }
+    if (!flag) {
+        objects.push_back(whoseColor);
+    }
 }
 
-void VisionWindow::onComboBoxSelectColorRobot2(Gtk::ComboBox* combobox) {
+void VisionWindow::onComboBoxSelectColorRobot2(Gtk::ComboBox *combobox) {
     vector<string> color = {"Green", "Pink", "Purple", "Red", "Brown"};
     ColorType colorType = toColorType(color[combobox->get_active_row_number()]);
     WhoseName whoseName = WhoseName::Robot2;
     WhoseColor whoseColor(whoseName, colorType);
-    objects.push_back(whoseColor);
+    bool flag = false;
+    for (unsigned i = 0; i < objects.size(); i++) {
+        if (whoseName == objects[i].whoseName) {
+            objects[i] = whoseColor;
+            flag = true;
+        }
+    }
+    if (!flag) {
+        objects.push_back(whoseColor);
+    }
 }
 
-void VisionWindow::onComboBoxSelectColorRobot3(Gtk::ComboBox* combobox) {
+void VisionWindow::onComboBoxSelectColorRobot3(Gtk::ComboBox *combobox) {
     vector<string> color = {"Green", "Pink", "Purple", "Red", "Brown"};
     ColorType colorType = toColorType(color[combobox->get_active_row_number()]);
     WhoseName whoseName = WhoseName::Robot3;
     WhoseColor whoseColor(whoseName, colorType);
-    objects.push_back(whoseColor);
+    bool flag = false;
+    for (unsigned i = 0; i < objects.size(); i++) {
+        if (whoseName == objects[i].whoseName) {
+            objects[i] = whoseColor;
+            flag = true;
+        }
+    }
+    if (!flag) {
+        objects.push_back(whoseColor);
+    }
 }
 
-void VisionWindow::onComboBoxSelectColorRobot4(Gtk::ComboBox* combobox) {
+void VisionWindow::onComboBoxSelectColorRobot4(Gtk::ComboBox *combobox) {
     vector<string> color = {"Green", "Pink", "Purple", "Red", "Brown"};
     ColorType colorType = toColorType(color[combobox->get_active_row_number()]);
     WhoseName whoseName = WhoseName::Robot4;
     WhoseColor whoseColor(whoseName, colorType);
-    objects.push_back(whoseColor);
+    bool flag = false;
+    for (unsigned i = 0; i < objects.size(); i++) {
+        if (whoseName == objects[i].whoseName) {
+            objects[i] = whoseColor;
+            flag = true;
+        }
+    }
+    if (!flag) {
+        objects.push_back(whoseColor);
+    }
 }
 
-void VisionWindow::onComboBoxSelectColorRobot5(Gtk::ComboBox* combobox) {
+void VisionWindow::onComboBoxSelectColorRobot5(Gtk::ComboBox *combobox) {
     vector<string> color = {"Green", "Pink", "Purple", "Red", "Brown"};
     ColorType colorType = toColorType(color[combobox->get_active_row_number()]);
     WhoseName whoseName = WhoseName::Robot5;
     WhoseColor whoseColor(whoseName, colorType);
-    objects.push_back(whoseColor);
+    bool flag = false;
+    for (unsigned i = 0; i < objects.size(); i++) {
+        if (whoseName == objects[i].whoseName) {
+            objects[i] = whoseColor;
+            flag = true;
+        }
+    }
+    if (!flag) {
+        objects.push_back(whoseColor);
+    }
 }
