699223859acc193f28fb84fd4bb7661527dca885
Manoel Stilpen <mstilpen15@gmail.com>
2018-05-14 14:55:14 -0300

    arquivos iniciais

---
+++ include/Interfaces/IRobotRecognizer.h
@@ -0,0 +1,18 @@
+//
+// Created by manoel on 14/05/18/.
+//
+
+#ifndef VSS_VISION_IROBOTRECOGNIZER_H
+#define VSS_VISION_IROBOTRECOGNIZER_H
+
+#include <vector>
+#include <cxcore.h>
+
+class IRobotRecognizer {
+	
+	
+	
+};
+
+#endif
+
---
+++ include/RobotRecognizer.h
@@ -0,0 +1,18 @@
+//
+// Created by manoel on 14/05/18/.
+//
+
+#ifndef VSS_VISION_ROBOTRECOGNIZER_H
+#define VSS_VISION_ROBOTRECOGNIZER_H
+
+#include "Interfaces/IRobotRecognizer.h"
+
+class RobotRecognizer : public IRobotRecognizer {
+	
+public:
+	
+	
+	
+};
+
+#endif
