2fc6425699fffe94c854b7ae29c1e78ffc84e9dd
Alexandre <alexandremstf@gmail.com>
2019-01-27 18:10:14 -0200

    Convertendo toda interface GTK para versão 3

--- src/Windows/Calibration/PartialCalibrationWindowSetup.cpp
+++ src/Windows/Calibration/PartialCalibrationWindowSetup.cpp
@@ -70,12 +70,6 @@ void CalibrationWindow::initializeWidget(){
 
     radioButtonImage->set_active();
 
-    // show only .txt files
-    auto filterText = fileChooserDialog->get_filter();
-    filterText->set_name("Text files");
-    filterText->add_pattern("*.txt");
-    //fileChooserDialog->add_filter(*filterText);
-
     // define initial folder for file chooser
     fileChooserDialog->set_current_folder(defaultFilesPath + "/data");
 
