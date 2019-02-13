4d93fa42ea77705e7ec4d5a034c32cf094f4f9ef
Alexandre <alexandremstf@gmail.com>
2019-02-13 15:47:29 -0200

    Atualizando arquivo de coeficientes da camera

--- src/CameraReader.cpp
+++ src/CameraReader.cpp
@@ -57,8 +57,8 @@ void CameraReader::initializeReceivement() {
         return;
     }
 
-    capture = cv::VideoCapture(0);
-    capture.set(cv::CAP_PROP_FOURCC,  cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    capture = cv::VideoCapture(1);
+    capture.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
 
     if (!capture.isOpened()) {
         std::cerr << "[Error] Camera cannot open" << std::endl;
